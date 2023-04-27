7b9aaff28bd9b0e43aae96ec71c0801c9d53a348
菊池悠 <124327954+Yuu-Kikuchi@users.noreply.github.com>
2023-04-27 14:11:33 +0900

    Add files via upload
    aaa

---
+++ js/main.js
@@ -0,0 +1,51 @@
+"use strict";
+
+//****************************************************** */
+//ハンバーガーメニュー
+//****************************************************** */
+
+//ハンバーガーメニューを取得
+const ham = document.getElementById("hamburger-menu");
+console.log(ham);
+
+//ヘッダーのグローバルナビゲーションメニューを取得
+const gnavM = document.querySelector(".header-gnav .gnav-menu");
+console.log(gnavM);
+
+//ハンバーガーメニューを押したときの処理
+ham.onclick = function () {
+    //右から流れるようにメニューが出現
+    gnavM.classList.toggle("show");
+    //ハンバーガーが×に変化
+    this.classList.toggle("cross");
+    //上下スクロールを無効
+    document.querySelector("body").classList.toggle("no-scroll");
+}
+
+//*********************************************** */
+//Topへ戻るボタン
+//*********************************************** */
+
+// トップへ戻るボタンを取得
+const pageTop = document.getElementById("pagetop-butn");
+console.log(pageTop);
+
+// クリックしたら一番上までスクロールするイベントを追加
+pageTop.addEventListener("click", scroll_top);
+
+// 一番上へスムーズにスクロールさせるための関数を定義
+function scroll_top() {
+    window.scroll({ top: 0, behavior: "smooth" });
+}
+
+// 一定の所までスクロールされたらボタンを表示
+window.addEventListener("scroll", topButn_event);
+// ボタンを表示させるスクロールイベントを定義
+function topButn_event() {
+    // 規定の位置に移動したらボタンを表示
+    if (window.pageYOffset > 400) {
+        pageTop.classList.add("show");
+    } else{
+        pageTop.classList.remove("show");
+    }
+}
---
+++ js/toppage.js
@@ -0,0 +1,66 @@
+"use strict";
+
+// ********************************* */
+// キービジュアル画像の切り替え処理
+//********************************** */
+
+let time = new Date();
+let TimeHours = time.getHours()
+console.log(TimeHours);
+
+let kVisual;
+
+function keyVisual(){
+    let kVisual = document.getElementById("key-visual");
+    console.log(kVisual);
+
+    if (TimeHours > 5 && TimeHours < 18) {
+    kVisual.style.backgroundImage = "url(./images/bg_tokushima_noon.jpg)";
+    } else {
+    kVisual.style.backgroundImage = "url(./images/bg_tokushima_night.jpg)";
+    }
+
+    // 5時以降、指定時刻が来たタイミングで背景が切り替わる処理(夜→昼)
+    if (TimeHours === 5) {
+        setTimeout(function () {
+            kVisual.style.backgroundImage = "url(./images/bg_tokushima_night.jpg)";
+        }, new Date().setHours(6, 0, 0, 0) - time);
+    }
+
+
+//   17時以降、指定時刻が来たタイミングで背景が切り替わる処理(昼→夜)
+    if (TimeHours === 17) {
+        setTimeout(function () {
+            kVisual.style.backgroundImage = "url(./images/bg_tokushima_night.jpg)";
+        }, new Date().setHours(18, 0, 0, 0) - time);
+    }
+}
+
+keyVisual();
+
+
+// ********************************* */
+// Works、Blogのコンテンツをスクロールでフェードインさせる処理
+//********************************** */
+
+// フェードインをさせるコンテンツを取得
+const fadeinTarget = document.querySelectorAll(".fade");
+
+// スクロールでフェードインイベントの実行
+window.addEventListener("scroll", fadeinBoxes);
+
+// 関数の定義
+function fadeinBoxes() {
+    //発火位置の指定
+    const triggerBottom = window.innerHeight;
+
+    fadeinTarget.forEach((box) => {
+        //コンテンツ(box)の頭の部分の座標を取得
+        const boxTop = box.getBoundingClientRect().top;
+
+        //boxの頭部分が発火位置を超えたら処理実行
+        if (boxTop < triggerBottom) {
+            box.classList.add("open");
+        }
+    });
+}
